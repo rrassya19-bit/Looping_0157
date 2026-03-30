1620e7a435de564074152c1cd5c4be6b61a3cfc5
rrassya19-bit <rrassys19@gmail.com>
2026-03-30 20:19:55 +0700

    menambahkan Logika OR

--- looping.cpp
+++ looping.cpp
@@ -57,3 +57,18 @@ int main(){
         status = "Tidak lulus";
 
     cout<<"Status kelulusan = "<<status<<" ,dengan nilai rata-rata = "<<rerata<<endl;
+
+    // Logika OR
+    cout<<"Masukkan nilai Matematika = ";
+    cin>>nilM;
+    cout<<"Masukkan nilai Bahasa Inggris = ";
+    cin>>nilB;
+
+    rerata = (nilB+nilM)/2;
+
+    if ((rerata >= 60) || (nilM >= 70))
+        status = "Lulus";
+    else
+        status = "Tidak lulus";
+
+    cout<<"Status kelulusan = "<<status<<" ,dengan nilai rata-rata = "<<rerata<<endl;
